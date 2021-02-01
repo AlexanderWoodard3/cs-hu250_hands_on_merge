5bc72517e16eb198db32fe6249df370c1f612118
Alexander Woodard <alexanderwoodard@u.boisestate.edu>
2021-01-31 19:33:53 -0700

    revision three

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 public class Main
 {
-    //this is the chronological revision 2 in branch-feature
+    //this is the chronological revision 3 in master
     public static void main(String[] args)
     {
-        System.out.println("Hello world");    
-     }
-    public void methodInBranch()
+        System.out.println("Hello world");
+    }
+    public void methodInMaster()
     {
-        System.out.println("Method in branch");
+        System.out.println("Method in master");
     }
 }
